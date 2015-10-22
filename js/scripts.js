e233a76e6ef47a7ae16f9f8c84d007c77f5bf845
Mya Lwin & Audrey Scrugham <student@epicodus.com>
2015-10-21 20:41:32 -0700

    tentively pass isVowel spec, again.

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,9 @@ var isVowel = function(string) {
   if(string.charAt(0) === "a" || string.charAt(0) === "e" || string.charAt(0) === "i" || string.charAt(0) === "o" || string.charAt(0) === "u") {
     return string + "ay";
   } else {
-    return string
+    //var vowel = ["a","e","i","o","u"]
+    var consonant = string.slice(0,1)
+    var string = string.slice(1)
+    return string + consonant + "ay"
   }
 }
--- specs/specs.js
+++ specs/specs.js
@@ -2,4 +2,7 @@ describe("isVowel", function() {
   it("Is a word that starts with 'a', 'e', 'i', 'o', or 'u'", function(){
     expect(isVowel("air")).to.equal("airay")
   });
+  it("Is a word that does not start with 'a', 'e', 'i', 'o', or 'u'", function(){
+    expect(isVowel("hair")).to.equal("airhay")
+  });
 });
